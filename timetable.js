c97bc2718312581c24b9a6cc79b6f8a83b99f44f
Yevgen Reminetski <yevgen4@gmail.com>
2018-05-29 13:04:38 +0200

    identation

--- timetable.js
+++ timetable.js
@@ -1,46 +1,26 @@
-$(document).ready(function() {
-
-	var week = {
-    	mondayActivity:[],
-    	tuesdayActivity:[],
-    	wednesdayActivity:[],
-    	thursdayActivity:[],
-    	fridayActivity:[],
-    	saturdayActivity:[],
-    	sundayActivity:[]
-	};
-
-    $(".addActivity").click(function(eve){ 
-
-		var text = $('#'+this.dataset.input).val();
-
+$(document).ready(function () {
+    
+    var week = {
+        mondayActivity: [],
+        tuesdayActivity: [],
+        wednesdayActivity: [],
+        thursdayActivity: [],
+        fridayActivity: [],
+        saturdayActivity: [],
+        sundayActivity: []
+    };
+    
+    $(".addActivity").click(function (eve){
+        var text = $('#'+this.dataset.input).val();
         $(this.dataset.container).append("<p>"+text+"</p>");
-
         $(this.dataset.container+" "+"p").hide();
-
-		$(this.dataset.container+" "+"p").fadeIn("slow");
-       
+        $(this.dataset.container+" "+"p").fadeIn("slow");
         week[this.dataset.input].push(text);
-
-        console.log(week[this.dataset.input],this.dataset.input);
-        
-	});
-
-
-	$(".cleanDay").click(function(eve){
-
-		$(this.dataset.box).slideUp("slow",function() {
-			$(this.dataset.box+" "+"p").remove();            //have a callback from a slideUp, does hide() after slide
-		});
-
- 		// $(this.dataset.box+" "+"p").hide();
-
-	});
-
-
-
-	
-	//console.log('DOС ready');
+    });
+    
+    $(".cleanDay").click(function (eve){
+        $(this.dataset.box).slideUp("slow",function () {
+            $(this.dataset.box+" "+"p").remove(); // have a callback from a slideUp, does hide() after slide
+        });
+    });
 });
-	//console.log('DOC not ready yet');
-
